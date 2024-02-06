8f21a58fe228a75622f12054e4f5b0b1cf126a9a
Silence-Mac <princedibloni4@gmail.com>
2024-02-06 03:05:29 +0000

    doing

--- client/alladd.js
+++ client/alladd.js
@@ -26,9 +26,9 @@ const getAnnonce = async (method, endpoint, data = null) => {
     const items = await getAnnonce('GET', 'boutique/annoncedata/nuance');
 
     if (items && items.length > 0) {
-        $("#logointerne").attr("src", `${items.find(item => item.which === "logointern") ? items.find(item => item.which === "logointern").image : "../assets/img/logo.png"}`);
+        $("#logointerne").attr("src", `${items.find(item => item.which === "logointern") ? items.find(item => item.which === "logointern").image : "/assets/img/logo.png"}`);
     } else {
-        $("#logointerne").attr("src", "../assets/img/logo.png");
+        $("#logointerne").attr("src", "/assets/img/logo.png");
     }
 
 })(jQuery);
