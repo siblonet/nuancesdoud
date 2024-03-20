7232786d981d0ae4bdf3987783af5ca64d74049c
John Kokar <princedibloni4@gmail.com>
2024-03-20 10:45:40 +0000

    doinf

--- functionServices/recentproduct.js
+++ functionServices/recentproduct.js
@@ -166,10 +166,10 @@ function recentProduct(recenPr, ADA) {
                                 <h3><a href="detaila?ov=${product._id}" style="color: ${product.addcoul.substring(0, 7)};">${product.addarticle}</a></h3>
                                 <div class="star-rating">
                                     <i class="bx bxs-star"></i>
-                                    <i class="bx bxs-star" style="color: ${product.addcoul.substring(0, 7)} !important"></i>
-                                    <i class="bx bxs-star" style="color: ${product.addcoul.substring(8, 15)} !important"></i>
-                                    <i class="bx bxs-star" style="color: ${product.addcoul.substring(16, 23)} !important"></i>
-                                    <i class="bx bxs-star" style="color: ${product.addcoul.substring(24, 31)} !important"></i>
+                                    <i class="bx bxs-star"></i>
+                                    <i class="bx bxs-star"></i>
+                                    <i class="bx bxs-star"></i>
+                                    <i class="bx bxs-star"></i>
                                 </div>
                                 <div class="price">
                                 ${product.addreduction > 0 && product.addreduction < product.addprix ?
@@ -290,10 +290,10 @@ function recentProduct(recenPr, ADA) {
                     <h3><a href="detaila?ov=${product._id}" style="color: ${product.addreduction > 0 ? "#000000" : product.addcoul.substring(0, 7)};">${product.addarticle}</a></h3>
                     <div class="star-rating">
                         <i class="bx bxs-star"></i>
-                        <i class="bx bxs-star" style="color: ${product.addcoul.substring(0, 7)} !important"></i>
-                        <i class="bx bxs-star" style="color: ${product.addcoul.substring(8, 15)} !important"></i>
-                        <i class="bx bxs-star" style="color: ${product.addcoul.substring(16, 23)} !important"></i>
-                        <i class="bx bxs-star" style="color: ${product.addcoul.substring(24, 31)} !important"></i>
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
+                        <i class="bx bxs-star"></i>
                     </div>
                     <div class="price">
                     ${product.addreduction > 0 && product.addreduction < product.addprix ?
