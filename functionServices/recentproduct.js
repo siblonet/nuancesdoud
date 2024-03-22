74ad9d72eb20c4832ae16ffac11fef7773a988b1
John Kokar <princedibloni4@gmail.com>
2024-03-22 23:38:52 +0000

    doinff

--- functionServices/recentproduct.js
+++ functionServices/recentproduct.js
@@ -510,11 +510,11 @@ async function showProductQuickView(a, productId) {
 
         document.getElementById('quickViewNewPrice').innerText = product.addreduction > 0 && product.addreduction < product.addprix ? `${(product.addreduction / 1000).toFixed(3)} F.CFA` : `${(product.addprix / 1000).toFixed(3)} F.CFA`;
         
-        $('.rating .one').css('color', `${product.addcoul.substring(0, 7)} `);
+        /*$('.rating .one').css('color', `${product.addcoul.substring(0, 7)} `);
         $('.rating .two').css('color', `${product.addcoul.substring(8, 15)} `);
         $('.rating .tree').css('color', `${product.addcoul.substring(16, 23)} `);
         $('.rating .four').css('color', `${product.addcoul.substring(24, 31)} `);
-        document.getElementById('rating').innerText = `5 étoile`;
+        document.getElementById('rating').innerText = `5 étoile`;*/
         document.getElementById('descrip').innerText = product.notes.length > 9 ? product.notes : "";
 
         let prodque = document.getElementById('productQuantity');
