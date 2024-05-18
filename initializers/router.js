4182ba2c3ce616f64c188e508576e61d2406c760
John Kokar <princedibloni4@gmail.com>
2024-05-18 08:59:13 -0700

    dfra

--- initializers/router.js
+++ initializers/router.js
@@ -1,5 +1,5 @@
-const apiUrlfine = 'http://localhost:3000/';
-const apiUrlfin = 'https://nuance-doud.adaptable.app/';
+const apiUrlfin = 'http://localhost:3000/';
+const apiUrlfine = 'https://nuance-doud.adaptable.app/';
 const tunalS = "https://liveshopping.adaptable.app/"
 const tunals = "http://localhost:3000/";
 const cinetpay = 'https://api-checkout.cinetpay.com/v2/payment';
