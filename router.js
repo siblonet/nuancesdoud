ccf64a846e8e26c794148753ad2cb181b3eba778
siblonet <70165373+siblonet@users.noreply.github.com>
2025-03-07 16:58:19 -0700

    Backend url changes
    Changed the backend url from adapter.io to render.com's url

--- router.js
+++ router.js
@@ -1,5 +1,7 @@
 const apiUrlfin = 'http://localhost:3000/';
-const apiUrlfine = 'https://nuance-doud.adaptable.app/';
+const apiUrlfine = 'https://multiple-backend-wh4q.onrender.com/';
 const tunalS = "https://liveshopping.adaptable.app/"
 const tunals = "http://localhost:3000/";
 const cinetpay = 'https://api-checkout.cinetpay.com/v2/payment';
+
+// old url:https://nuance-doud.adaptable.app/
